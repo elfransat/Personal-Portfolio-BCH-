57c0c9246a951dd150a805c6f7f1d3997cc7244f
Louis Atkinson <tietohallinto@BC-minimac041.local>
2020-10-05 14:45:07 +0300

    ui changes

--- script.js
+++ script.js
@@ -16,3 +16,14 @@ const topFunction = () => {
   document.body.scrolltop = 0; // for Safari
   document.documentElement.scrollTop = 0; //Other browsers
 };
+
+/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "flex") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "flex";
+    x.style.flexDirection = "column";
+  }
+}
